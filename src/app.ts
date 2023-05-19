a2692ab984b106467e3ecf472e555558efb4f0bd
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-05-19 22:31:12 +0300

    add serverless to express app

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import "express-async-errors";
+
+import express, { json } from "express";
+import helmet from "helmet";
+
+const app = express();
+app.use(json());
+app.use(helmet());
+
+app.get("/", (_, res) => {
+  res.json({
+    msg: "Hello World",
+  });
+});
+
+app.get("/go", async (_, res) => {
+  res.json({
+    msg: "Add a new unique user without duplicate",
+  });
+});
+
+app.use((_, res, _2) => {
+  res.status(404).json({ error: "NOT FOUND" });
+});
+
+export { app };
